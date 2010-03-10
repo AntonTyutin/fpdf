da679e57b16c344789a8e7d34121b4343d4d0241
Lars Olesen <lars@intraface.dk>
2010-03-11 00:20:01 +0100

    Added bookmarks example.

---
+++ examples/bookmark/ex.php
@@ -0,0 +1,20 @@
+<?php
+require('bookmark.php');
+
+$pdf=new PDF_Bookmark();
+$pdf->SetFont('Arial','',15);
+//Page 1
+$pdf->AddPage();
+$pdf->Bookmark('Page 1');
+$pdf->Bookmark('Paragraph 1',1,-1);
+$pdf->Cell(0,6,'Paragraph 1');
+$pdf->Ln(50);
+$pdf->Bookmark('Paragraph 2',1,-1);
+$pdf->Cell(0,6,'Paragraph 2');
+//Page 2
+$pdf->AddPage();
+$pdf->Bookmark('Page 2');
+$pdf->Bookmark('Paragraph 3',1,-1);
+$pdf->Cell(0,6,'Paragraph 3');
+$pdf->Output();
+?>
