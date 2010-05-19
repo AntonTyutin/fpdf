526206c705d9aa1ae7fb8d8d8746912b34554027
Lars Olesen <lars@intraface.dk>
2010-05-19 16:49:20 +0200

    Started work on some smoketests.

--- examples/cell/ex.php
+++ examples/cell/ex.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../../src/fpdf.php';
 require('cellpdf.php');
 
 $pdf=new CellPDF();
--- tests/Test.php
+++ tests/Test.php
@@ -7,4 +7,29 @@ class Test extends PHPUnit_Framework_TestCase
     {
         $this->assertTrue(is_object(new FPDF));
     }
+
+    function testCell()
+    {
+        require_once '../examples/cell/cellpdf.php';
+        $pdf=new CellPDF();
+        $pdf->AddPage();
+        $pdf->SetFont('Arial','',12);
+
+        $pdf->VCell(15,50,"Text at\nbottom",1,0,'D');
+        $pdf->VCell(10,50,'Centered text',2,0,'C');
+        $pdf->VCell(15,50,"Text\non top",1,0,'U');
+
+        $pdf->Cell(50,50,"Text on\nthe left",'lbtR',0,'L');
+        $pdf->Cell(50,50,'This line is very long and gets compressed','LtRb',0,'C');
+        $pdf->Cell(50,50,"Text on\nthe right",'Ltrb',0,'R');
+
+        $pdf->Output('doc.pdf', 'F');
+
+        $content = file_get_contents('doc.pdf');
+        $expected_content = file_get_contents(dirname(__FILE__) . '/../examples/cell/ex.pdf');
+
+        // @todo how can I take the creation date into consideration
+
+        $this->assertEquals($expected_content, $content);
+    }
 }
